4472d42b8777cb5d30bdfb7e1225266378e87454
wraithchaser91 <up629021@myport.ac.uk>
2020-07-05 19:23:50 +0100

    Fixed API Call

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {errorLog,variables} = require("../utils.js");
 
 router.get("/", (req,res)=>{
-    res.sendJSON({"test":"1"});
+    res.send({"test":"1"});
 });
 
 module.exports = router;
