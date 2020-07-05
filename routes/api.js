74ad8a5dd5e9c28281225a2b6e5cc8b958b1a7ed
wraithchaser91 <up629021@myport.ac.uk>
2020-07-05 19:18:43 +0100

    API change

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {errorLog,variables} = require("../utils.js");
 
 router.get("/", (req,res)=>{
+    res.sendJSON({"test":"1"});
 });
 
 module.exports = router;
